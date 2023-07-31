356ad98db87b776fdac347d92812b6e315d3b35a
Elbert <elbertnatan@hotmail.com>
2023-07-31 11:05:06 -0300

    - mostrando lista

--- flutter_expenses_app/lib/expenses.dart
+++ flutter_expenses_app/lib/expenses.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:futter_expenses_app/Expense.dart';
+import 'package:futter_expenses_app/expenses_list.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -12,18 +15,26 @@ class Expenses extends StatefulWidget {
 
 class _ExpensesState extends State<Expenses> {
   final List<Expense> _registereExpenses = [
-    Expense(title: 'Flutter Courses', amount: 19.99, date: DateTime.now(), category: Category.work),
-    Expense(title: 'Cinema', amount: 14.99, date: DateTime.now(), category: Category.leisure)
+    Expense(
+        title: 'Flutter Courses',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Cinema',
+        amount: 14.99,
+        date: DateTime.now(),
+        category: Category.leisure)
   ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text("Chart"),
-          Text("List")
+          const Text("Chart"),
+          Expanded(child: ExpensesList(expenses: _registereExpenses))
         ],
       ),
     );
