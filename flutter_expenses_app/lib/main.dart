b7cc219a2de47bcd99a1afa90745a624883b66dd
Elbert <elbertnatan@hotmail.com>
2023-08-07 09:17:53 -0300

    - adicionando temas

--- flutter_expenses_app/lib/main.dart
+++ flutter_expenses_app/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:futter_expenses_app/widgets/expenses.dart';
 
 var kColorScheme =
-    ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
 
 void main() {
   runApp(
@@ -14,6 +14,22 @@ void main() {
           backgroundColor: kColorScheme.onPrimaryContainer,
           foregroundColor: kColorScheme.primaryContainer,
         ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: kColorScheme.onSecondaryContainer,
+                fontSize: 16,
+              ),
+            ),
       ),
       home: const Expenses(),
     ),
