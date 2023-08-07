aedf400af5aed673671d23b9d241e6705f91cfd7
Elbert <elbertnatan@hotmail.com>
2023-08-07 09:27:16 -0300

    - soma para grafico

--- flutter_expenses_app/lib/models/Expense.dart
+++ flutter_expenses_app/lib/models/Expense.dart
@@ -4,7 +4,6 @@ import 'package:intl/intl.dart';
 
 final formatter = DateFormat('dd/MM/yyyy');
 
-
 const uuid = Uuid();
 
 enum Category { food, travel, leisure, work }
@@ -17,7 +16,12 @@ const categoryIcons = {
 };
 
 class Expense {
-  Expense({required this.title, required this.amount, required this.date, required this.category}) : id = uuid.v4();
+  Expense(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.category})
+      : id = uuid.v4();
 
   final String id;
   final String title;
@@ -25,7 +29,24 @@ class Expense {
   final DateTime date;
   final Category category;
 
-  String get formattedDate{
+  String get formattedDate {
     return formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  const ExpenseBucket({required this.category, required this.expenses});
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get TotalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+
+    return sum;
   }
 }
